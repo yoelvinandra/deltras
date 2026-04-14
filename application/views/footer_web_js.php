5b140bb23604ed84c7ff5afdfbc5b4a76fce81de
yoel vndr <yoelvinandra@gmail.com>
2026-04-14 09:06:51 +0700

    [MINOR] Update kecil

--- application/views/footer_web_js.php
+++ application/views/footer_web_js.php
@@ -206,29 +206,36 @@ $.ajax({
 });
 
 function timeYoutubePublished(isoString) {
-  var published = new Date(isoString);
-  var now       = new Date();
-  var seconds   = Math.floor((now - published) / 1000);
+  if(isoString == "")
+  {
+    return "";
+  }
+  else
+  {
+    var published = new Date(isoString);
+    var now       = new Date();
+    var seconds   = Math.floor((now - published) / 1000);
 
-  if (seconds < 60)                        return 'Just now';
+    if (seconds < 60)                        return 'Just now';
 
-  var minutes = Math.floor(seconds / 60);
-  if (minutes < 60)                        return minutes + ' minute' + (minutes > 1 ? 's' : '') + ' ago';
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 60)                        return minutes + ' minute' + (minutes > 1 ? 's' : '') + ' ago';
 
-  var hours = Math.floor(minutes / 60);
-  if (hours < 24)                          return hours + ' hour' + (hours > 1 ? 's' : '') + ' ago';
+    var hours = Math.floor(minutes / 60);
+    if (hours < 24)                          return hours + ' hour' + (hours > 1 ? 's' : '') + ' ago';
 
-  var days = Math.floor(hours / 24);
-  if (days < 7)                            return days + ' day' + (days > 1 ? 's' : '') + ' ago';
+    var days = Math.floor(hours / 24);
+    if (days < 7)                            return days + ' day' + (days > 1 ? 's' : '') + ' ago';
 
-  var weeks = Math.floor(days / 7);
-  if (weeks < 4)                           return weeks + ' week' + (weeks > 1 ? 's' : '') + ' ago';
+    var weeks = Math.floor(days / 7);
+    if (weeks < 4)                           return weeks + ' week' + (weeks > 1 ? 's' : '') + ' ago';
 
-  var months = Math.floor(days / 30);
-  if (months < 12)                         return months + ' month' + (months > 1 ? 's' : '') + ' ago';
+    var months = Math.floor(days / 30);
+    if (months < 12)                         return months + ' month' + (months > 1 ? 's' : '') + ' ago';
 
-  var years = Math.floor(days / 365);
-  return years + ' year' + (years > 1 ? 's' : '') + ' ago';
+    var years = Math.floor(days / 365);
+    return years + ' year' + (years > 1 ? 's' : '') + ' ago';
+  }
 }
 
 function openModal(videoId, title) {
